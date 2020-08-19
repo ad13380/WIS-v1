3a0c0d374e07e5daf411813caf92834df186e447
Anthony <a.a.donovan@outlook.com>
2020-08-19 19:19:53 +0100

    adds basic 404 error page

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 
@@ -11,5 +12,18 @@ app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
 
 module.exports = app;
--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -1,10 +1,11 @@
 window.addEventListener(`DOMContentLoaded`, async function (event) {
   let controller = new Controller()
 
+  document.getElementById('preview-list').innerHTML = '<h1>Loading</h1>'
   document.getElementById('preview-list').innerHTML = await controller.returnListHtml();
   
   controller.profileIds.forEach(id => {
-    var node = document.createElement("DIV");
+    let node = document.createElement("DIV");
     node.innerHTML = controller.returnProfileHtml(id);
     document.getElementById('profile-modal').appendChild(node);
   })
