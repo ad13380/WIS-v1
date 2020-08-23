b4ac1d01824fa61d30139ba20ca52471768a3ba5
Anthony <a.a.donovan@outlook.com>
2020-08-23 21:36:20 +0100

    updated flash page

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-
 var indexRouter = require('./routes/index');
 
 const app = express();
@@ -13,20 +12,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 
-app.use('/test', (req, res) => {
-  res.render('test')
-})
-
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
   res.locals.error = err
-
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
